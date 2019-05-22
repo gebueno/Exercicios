522ea63def680418aa4e1c640c978d39079d37ed
Gerson <Gerson@192.168.15.5>
2019-05-22 01:02:58 -0300

    Apenas introduzi um início do exercício 4, amanhã eu continuo.

---
+++ TP07/src/exercicio4/MediaAlunos.java
@@ -0,0 +1,9 @@
+package exercicio4;
+
+public class MediaAlunos {
+	private static double mediatotal;
+    private static double[] notasAlunos = {10, 5, 7, 8, 6, 8, 10, 9, 7, 6, 7, 6, 8, 4, 6};
+    
+    
+
+}
