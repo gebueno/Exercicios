1b3f786a6b822aa0c570cb31706a3ee9e8d107de
Gerson <Gerson@192.168.15.5>
2019-05-22 12:11:49 -0300

    Resolvi a lógica do exercício 4

--- TP07/src/exercicio4/MediaAlunos.java
+++ TP07/src/exercicio4/MediaAlunos.java
@@ -4,6 +4,28 @@ public class MediaAlunos {
 	private static double mediatotal;
     private static double[] notasAlunos = {10, 5, 7, 8, 6, 8, 10, 9, 7, 6, 7, 6, 8, 4, 6};
     
+	public static double getMediatotal() {
+		return mediatotal;
+	}
+	public static void setMediatotal(double mediatotal) {
+		MediaAlunos.mediatotal = mediatotal;
+	}
+	public static double[] getNotasAlunos() {
+		return notasAlunos;
+	}
+	public static void setNotasAlunos(double[] notasAlunos) {
+		MediaAlunos.notasAlunos = notasAlunos;
+	}
     
+	public static double mediat(double notasAlunos[]) {
 
+        double soma = 0;
+        for (int i = 0; i < 15; i++) {
+            soma = soma + notasAlunos[i];
+            mediatotal = soma;
+        }
+        mediatotal = soma / 15;
+        return soma;
+
+}
 }
--- TP07/src/exercicio4/ResultadoMain.java
+++ TP07/src/exercicio4/ResultadoMain.java
@@ -3,8 +3,11 @@ package exercicio4;
 public class ResultadoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		MediaAlunos conta = new MediaAlunos();
+		
+		MediaAlunos.mediat(MediaAlunos.getNotasAlunos());		
+		System.out.println((MediaAlunos.getMediatotal()));
 	}
 
 }
